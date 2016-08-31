783272a40451286164b0d51c95e6cd99b087a755
Deyan Ginev <d.ginev@jacobs-university.de>
2016-08-31 14:33:19 +0300

    sync with texstyles 0.0.7 affiliation fix

--- test/style_hello_world_test.rb
+++ test/style_hello_world_test.rb
@@ -31,12 +31,12 @@ class StyleHelloWorldTest < Minitest::Test
     body = 'Hello \world!'
     header = '\def\world{World}'
 
-    styled_body = stylist.render(body, header, @example_metadata)
+    styled_doc = stylist.render(body, header, @example_metadata)
 
-    assert styled_body.include?(body), 'content was passed in'
-    assert styled_body.match(/\\usepackage\{graphicx\}/), 'default graphicx package is on'
-    assert styled_body.match(/\\begin\{document\}/), 'document start exists'
-    assert styled_body.match(/\\end\{document\}/), 'document end exists'
+    assert styled_doc.include?(body), 'content was passed in'
+    assert styled_doc.match(/\\usepackage\{graphicx\}/), 'default graphicx package is on'
+    assert styled_doc.match(/\\begin\{document\}/), 'document start exists'
+    assert styled_doc.match(/\\end\{document\}/), 'document end exists'
   end
 
   def test_can_auto_internationalize_cyrillic
@@ -45,11 +45,11 @@ class StyleHelloWorldTest < Minitest::Test
     body = 'Hello \world! Здравей свят! Done.'
     header = '\def\world{World}'
 
-    styled_body = stylist.render(body, header, @example_metadata)
+    styled_doc = stylist.render(body, header, @example_metadata)
 
-    assert styled_body.include?('Здравей свят'), 'cyrillic passed as is'
-    assert styled_body.include?('\\usepackage[russian,english]{babel}')
-    assert styled_body.include?('\\selectlanguage{russian}'), 'cyrillic activated'
-    assert styled_body.include?('\\selectlanguage{english}'), 'english activated'
+    assert styled_doc.include?('Здравей свят'), 'cyrillic passed as is'
+    assert styled_doc.include?('\\usepackage[russian,english]{babel}')
+    assert styled_doc.include?('\\selectlanguage{russian}'), 'cyrillic activated'
+    assert styled_doc.include?('\\selectlanguage{english}'), 'english activated'
   end
 end
